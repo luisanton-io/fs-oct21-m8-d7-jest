ae1afdd76c2167a5eac61f3c8bc6484da83f295b
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-22 16:46:58 +0200

    first test id

--- src/products/index.js
+++ src/products/index.js
@@ -1,20 +1,35 @@
 import express from "express"
 import Product from "./model.js"
-const productsRouter = express.Router();
+const productsRouter = express.Router()
 
 productsRouter
-    .get('/', async (req, res) => {
-        const products = await Product.find({});
-        res.status(200).send(products);
-    })
-    .post('/', async (req, res) => {
-        try {
-            const product = new Product(req.body)
-            await product.save()
-            res.status(201).send(product)
-        } catch (error) {
-            res.status(400).send(error)
-        }
-    })
+  .get("/", async (req, res) => {
+    const products = await Product.find({})
+    res.status(200).send(products)
+  })
+
+  .get("/:productId", async (req, res, next) => {
+    try {
+      const productId = req.params.productId
+      const product = await Product.findById(productId)
+
+      if (product) {
+        res.status(200).send(product)
+      } else {
+        res.status(404).send({ message: "invalid productId" })
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+  .post("/", async (req, res) => {
+    try {
+      const product = new Product(req.body)
+      await product.save()
+      res.status(201).send(product)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  })
 
 export default productsRouter
